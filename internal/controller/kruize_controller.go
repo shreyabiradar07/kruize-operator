7cff084cae00934afa8a16a1a1a367d28af45922
Shreya Biradar <shbirada@ibm.com>
2025-09-05 19:37:42 +0530

    Fix headers

--- internal/controller/kruize_controller.go
+++ internal/controller/kruize_controller.go
@@ -711,8 +711,6 @@ data:
             location /dsmetadata {
                 proxy_pass http://kruize_backend/dsmetadata;
                 proxy_set_header Host $host;
-                proxy_set_header X-Real-IP $remote_addr;
-                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             }
 
             # Serve static files for everything else
